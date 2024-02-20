8648dcd464907c681a7bc8fd9f927f11dc61dbe6
kartzum <kartzum@gmail.com>
2024-02-20 18:02:53 +0300

    j17t1.

---
+++ j17t1/src/main/java/io/rdlab/j17t1/Main.java
@@ -0,0 +1,8 @@
+package io.rdlab.j17t1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ j17t1/src/test/java/io/rdlab/j17t1/MainTest.java
@@ -0,0 +1,12 @@
+package io.rdlab.j17t1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertThat(true).isEqualTo(true);
+    }
+}
