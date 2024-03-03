2ca9c45f81ba41c589423915939766e3c0225b5b
kartzum <kartzum@gmail.com>
2024-03-03 20:11:08 +0300

    a.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Tasks.
 
 ## kt1.
 Tasks.
+
+## jlcsb17t1.
+Tasks. Java 17 + SP + postgresql.
---
+++ jlcsb17t1/src/main/java/xzfg/ex/jlcsb17t1/Jlcsb17t1Application.java
@@ -0,0 +1,13 @@
+package xzfg.ex.jlcsb17t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jlcsb17t1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jlcsb17t1Application.class, args);
+	}
+
+}
